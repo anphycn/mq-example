3ddb9bf3457b1ad2bdaacbb5f8a49bf866f8f135
anphycn <anphycn@gmail.com>
2019-05-21 00:29:37 +0800

    111

---
+++ rocket-mq/src/main/java/com/github/anphycn/pojo/ResponseMessage.java
@@ -0,0 +1,13 @@
+package com.github.anphycn.pojo;
+
+public class ResponseMessage {
+    private String responseMessage;
+
+    public String getResponseMessage() {
+        return responseMessage;
+    }
+
+    public void setResponseMessage(String responseMessage) {
+        this.responseMessage = responseMessage;
+    }
+}
