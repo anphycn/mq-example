90607df1dce7987735369adf5b59730f7e52fcc9
anyw <anyw@doone.com.cn>
2019-05-22 14:39:30 +0800

    333

--- rabbit-mq/src/main/java/com/github/anphycn/helloworld/HelloWorldQueueConfig.java
+++ rabbit-mq/src/main/java/com/github/anphycn/helloworld/HelloWorldQueueConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class HelloWorldQueueRabbitConfig {
+public class HelloWorldQueueConfig {
 
     @Bean
     public Queue HelloWorldQueue() {
